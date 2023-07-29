1f76f4c83d21b101dbf3c88509a04e8f99f5bb4b
mrtoffdev <pub.mrtoff@proton.me>
2023-07-30 05:22:39 +0800

    Fix: Fixed bug where files are improperly validated due to incomplete source paths

--- src/core/vult_core.py
+++ src/core/vult_core.py
@@ -6,7 +6,8 @@ from util.dev_utils import log
 
 class Core:
         @staticmethod
-        def validate_file(file: str | Path) -> bool:
+        def validate_file(source_dir: str | Path, file: str | Path) -> bool:
+                log("fs", "bs file found & validating: " + str(file))
                 # Vult Supported Formats
                 SUPPORTED_FORMATS = [
                     ".mp4",
@@ -15,8 +16,12 @@ class Core:
                     ".avi"
                 ]
 
+                log("fs", "bs file check: " + str(os.path.isfile(str(source_dir) +
+                                                                 str(file))))
+                log("fs", "bs split result: " + str(os.path.splitext(file)))
+
                 # Validate
-                if  os.path.isfile(file) and \
+                if  os.path.isfile(str(source_dir) + str(file)) and \
                     os.path.splitext(file)[1] in SUPPORTED_FORMATS:
                         return True
                 else:
@@ -24,16 +29,19 @@ class Core:
 
         @staticmethod
         def build_sources(source_dir: str | Path) -> [(str, str)]:
+                log("fs", "bs path rcv: " + source_dir)
+
                 paths = [
-                        file for file in os.listdir(source_dir)
-                        if Core.validate_file(file)
+                        file for file in os.listdir(Path(source_dir)) if
+                        Core.validate_file(source_dir, file)
                 ]
+                log("fs", "paths: " + str(paths))
                 sizes = []
 
                 log("logfile.txt", "From rebuild_sources: paths=" + str(paths))
 
                 for path in paths:
-                    sizes.append(str(os.path.getsize(path)))
+                    sizes.append(str(os.path.getsize(str(source_dir) + path)))
 
                 return list(zip(paths, sizes))
 
