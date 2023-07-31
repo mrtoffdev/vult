7d2df0da6fd0c71896893566267f7c0ae2ddf53c
mrtoffdev <pub.mrtoff@proton.me>
2023-07-31 17:23:47 +0800

    Chore: Cleaned spacing & added semi-useless inline commends so i don't forget the architecture again tomorrow

--- src/view/fm_panel.py
+++ src/view/fm_panel.py
@@ -4,23 +4,27 @@ from textual.app import ComposeResult
 from textual.widgets import Static
 from textual.containers import Container, Vertical
 
-from view.components.Search import FSearchWidget
+from view.components.InputForm import InputForm
 from view.components.Explorer import Explorer
 
 from util.dev_utils import log
 from core.vult_core import Core
 
 class FileChooserDialogue(Static):
-        CSS_PATH = "compress-scr.css"
+        # State
+        SOURCES: vec_T_str      = [("", "")]
 
-        SOURCES: vec_T_str = [("", "")]
-
-        LAYOUT = Static()
+        # DOM
+        CSS_PATH                = "compress-scr.css"
+        LAYOUT                  = Static()
 
         def rebuild_sources(self, source: str):
-            self.SOURCES = Core.build_sources(source)
+                self.SOURCES = Core.build_sources(source)
 
-        def compose(self) -> ComposeResult:
+        def __init__(self):
+                super().__init__()
+                log("log", "Init reached")
+
                 # Upper Section
                 SOURCE_TREE     = Explorer()
                 SOURCE_TREE.id  = "Source-dir"
@@ -36,11 +40,11 @@ class FileChooserDialogue(Static):
 
                 OUT_TREE.init_table(("", ""), [("", "")])
 
-                yield Container(
-                    # Video Sources [Upper Section]
+                self.LAYOUT     = Container(
+                        # Video Sources [Upper Section]
                         Vertical(
                                 Static("Video Sources Directory:", classes="dialogue-header-top"),
-                                FSearchWidget(),
+                                InputForm(),
                                 SOURCE_TREE,
 
                                 classes="FCD-section",
@@ -48,12 +52,17 @@ class FileChooserDialogue(Static):
 
                         # Output & Statistics [Lower Section]
                         Vertical(
-                                Static("Output Directory:", classes="dialogue-header-sub"),
-                                FSearchWidget(),
+                                Static( "Output Directory:",
+                                        classes="dialogue-header-sub"),
+                                InputForm(),
 
-                                Static("Results:", classes="dialogue-header-sub"),
+                                Static("Results:",
+                                        classes="dialogue-header-sub"),
                                 OUT_TREE,
 
                                 classes="FCD-section",
                         )
                 )
+
+        def compose(self) -> ComposeResult:
+                yield self.LAYOUT
