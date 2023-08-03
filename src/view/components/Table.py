fe5b913dcf4aaa164f4f0a6728c36597a04fe734
mrtoffdev <pub.mrtoff@proton.me>
2023-08-03 17:50:14 +0800

    Refactor: Revised TableEntry widget construction pipeline for better input coverage

--- src/view/components/Table.py
+++ src/view/components/Table.py
@@ -18,16 +18,22 @@ class TableEntry(Static):
         LAYOUT          = Static()
 
         def __init__(self, entry: T_str = None):
+                self.parse_cfg(entry)
                 super().__init__()
-                if entry is not None:
-                        self.LAYOUT = TableEntry.__build_component(entry)
-                else:
-                        self.LAYOUT = TableEntry.__build_component(self.VALUE)
 
-        @staticmethod
-        def __build_component(entry: T_str):
+        def parse_cfg(self, entry: T_str):
+                build = None
+                match entry:
+                        case entry if type(entry) == T_str:
+                                build = entry
+                        case None:
+                                build = TableEntry.VALUE
 
-                return Horizontal(
+                self.__build_component(build)
+
+        def __build_component(this, entry: T_str | None):
+
+                this.LAYOUT = Horizontal(
                         Static(
                                 TableEntry.ICON_1 +
                                 str(entry[0]),
@@ -45,16 +51,14 @@ class TableEntry(Static):
                         id=str(entry[0].rstrip(".mp4"))
                 )
 
-        # Todo: (set_entry()) Figure out a better way of re-rendering
-        #       the entry than rebuilding the whole component
-        def set_entry(self, entry: T_str) -> None:
-                self.LAYOUT = TableEntry.__build_component(entry)
-
         def compose(self) -> ComposeResult:
                 if self.LAYOUT == ():
                         yield Static("Invalid Entry: Default Entry Spawned")
                 else:
                         yield self.LAYOUT
+
+        def set_entry(self, entry: T_str) -> None:
+                self.LAYOUT = TableEntry.__build_component(entry)
 
 class TableHeader(Static):
         # Header State
