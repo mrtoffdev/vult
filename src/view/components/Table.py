9b6eeaaa75e58eb4fefb656f6f44254ece078a01
mrtoffdev <pub.mrtoff@proton.me>
2023-08-04 15:31:58 +0800

    Refactor: Revised Table widget construction pipeline for better input coverage

--- src/view/components/Table.py
+++ src/view/components/Table.py
@@ -1,9 +1,11 @@
 from textual.app import ComposeResult
+from textual.reactive import Reactive
 from textual.widget import Widget
 from textual.widgets import Static
-from textual.containers import Horizontal
+from textual.containers import Horizontal, Vertical
 
-from core.typedef import T_str
+from core.typedef import T_str, vec_T_str
+from util.dev_utils import log
 
 class TableEntry(Static):
         # Default Configuration
@@ -15,7 +17,7 @@ class TableEntry(Static):
 
         # Layout Props
         ENTRY_ID        = VALUE[0]
-        LAYOUT          = Static()
+        LAYOUT          = Reactive(Static())
 
         def __init__(self, entry: T_str = None):
                 self.parse_cfg(entry)
@@ -32,7 +34,6 @@ class TableEntry(Static):
                 self.__build_component(build)
 
         def __build_component(this, entry: T_str | None):
-
                 this.LAYOUT = Horizontal(
                         Static(
                                 TableEntry.ICON_1 +
@@ -51,14 +52,15 @@ class TableEntry(Static):
                         id=str(entry[0].rstrip(".mp4"))
                 )
 
+
         def compose(self) -> ComposeResult:
-                if self.LAYOUT == ():
-                        yield Static("Invalid Entry: Default Entry Spawned")
-                else:
-                        yield self.LAYOUT
+                yield self.LAYOUT
+
+
+        #= State Mgmt ===============================================================
 
         def set_entry(self, entry: T_str) -> None:
-                self.LAYOUT = TableEntry.__build_component(entry)
+                self.__build_component(entry)
 
 class TableHeader(Static):
         # Header State
@@ -97,11 +99,66 @@ class TableHeader(Static):
         def compose(self) -> ComposeResult:
                 yield self.LAYOUT
 
+
+# Typing
+TableConfig     = tuple[T_str, vec_T_str] | dict | None
+
 class Table(Widget):
-        """
-        # Table Widget
-        """
-        LAYOUT          = Static()
+
+        # State
+        HEADER          = Reactive(("", ""))
+        ENTRIES         = Reactive([("", "")])
+
+        WIDGET_HEADER   = Static()
+        SHOW_HEADER     = False
+
+        # DOM
+        HEADER_LAYOUT   = Static()
+        TABLE_LAYOUT    = Static()
+
+        LAYOUT          = Reactive(Static())
+
+        def __init__(self, config: TableConfig, *children: Widget):
+                self.parse_cfg(config)
+                super().__init__(*children)
+
+        def parse_cfg(this, config: TableConfig):
+                match config:
+                        # Tuple initialization
+                        case config if type(config) == tuple[T_str, vec_T_str]:
+                                this.HEADER = config[0] if (config[0] is not "" or
+                                        config[0] is not None) else None
+
+                        # Dict initialization
+                        case config if type(config) == dict:
+                                this.HEADER = config['header'] \
+                                        if (config['header'] is not "" or
+                                            config['header'] is not None) \
+                                        else TableHeader.DEFAULT
+
+                                this.ENTRIES = config['entries'] \
+                                        if (config['entries'] is not "" or
+                                            config['entries'] is not None) \
+                                        else None
+
+                this.__build_component()
+
+        def __build_component(this, config=None):
+                if config is None:
+                        pass
+                else:
+                        this.HEADER_LAYOUT      = TableHeader(this.HEADER)
+                        this.TABLE_LAYOUT       = Vertical()
+
+                        # Dynamically mount entries
+                        for entry in this.ENTRIES:
+                                this.TABLE_LAYOUT.mount(TableEntry(entry=entry))
+
+                        this.LAYOUT = Vertical (
+                                this.HEADER_LAYOUT,
+                                this.TABLE_LAYOUT
+                        )
+
 
         def compose(self) -> ComposeResult:
                 yield self.LAYOUT
