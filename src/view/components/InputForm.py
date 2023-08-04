97adb9d25fcee9907f516a6f809e2801cfab4e80
mrtoffdev <pub.mrtoff@proton.me>
2023-08-04 20:43:25 +0800

    Refactor: Revised InputForm widget construction pipeline for better input coverage

--- src/view/components/InputForm.py
+++ src/view/components/InputForm.py
@@ -45,28 +45,56 @@ class InputForm(Static):
         LAYOUT          = Static("FSearchWidget.LAYOUT err")
 
         def __init__(self, config=CONFIG, id=None):
+                self.parse_cfg(config)
                 super().__init__(id=id)
-                self.CONFIG     = config
-                self.__build_component()
 
+        # Helper for resolving config values & defaulting when null
         @staticmethod
-        def __parse_cfg(config: dict | IInputForm):
+        def __resolve(key: str, config: dict | IInputForm):
+                def __nullish(value, default) :
+                        if      (value is None) or \
+                                (value == "") or \
+                                (value == '') or \
+                                (value == ()):
+                                return default
+                        else:
+                                return value
+
+                match key:
+                        case 'header' if 'header' in config.keys():
+                                return __nullish(config['header'],
+                                                InputForm.CONFIG.HEADER)
+
+                        case 's_icon' if 's_icon' in config.keys():
+                                return __nullish(config['s_icon'],
+                                                InputForm.CONFIG.SEARCH_ICON)
+
+                        case 'hint' if 'hint' in config.keys():
+                                return __nullish(config['hint'],
+                                                InputForm.CONFIG.HINT)
+
+                        case 'h_icon' if 'h_icon' in config.keys():
+                                return __nullish(config['h_icon'],
+                                                InputForm.CONFIG.HINT_ICON)
+
+        def parse_cfg(this, config: dict | IInputForm):
                 log("log", f"__parse rcvd: {str(config)}")
+
+                # Dict initialization
                 if type(config) == dict:
-                        return Vertical(
-                                Static(f"{config['header']}:",
-                                       classes="dialogue-header-top"),
-                                Horizontal(
-                                        Input(placeholder=config["hint"], id="Search-field"),
-                                        Button(f"[{config['s_icon']}] Search", id="Search-button"),
+                        this.CONFIG.HEADER      = this.__resolve('header',
+                                                                config=config)
+                        this.CONFIG.HINT        = this.__resolve(key='hint',
+                                                                config=config)
+                        this.CONFIG.SEARCH_ICON = this.__resolve(key='s_icon',
+                                                                config=config)
+                        this.CONFIG.HINT_ICON   = this.__resolve(key='h_icon',
+                                                                config=config)
 
-                                        id="Search-widget"
-                                ),
-                                classes="h-auto-w-fill"
-
-                        )
+                # Config initialization
                 elif type(config) == IInputForm:
-                        return Vertical(
+
+                        this.LAYOUT = Vertical(
                                 Static("Video Sources Directory:", classes="dialogue-header-top"),
                                 Horizontal(
                                         Input(placeholder=config.HINT, id="Search-field"),
@@ -78,23 +106,41 @@ class InputForm(Static):
                         )
 
 
-        def __build_component(self):
+        def __build_component(this):
                 log("log", f"IForm __build()")
-                if self.CONFIG != None:
-                        log("log", f"IForm __build(): has config: "
-                                   f"XX")
-                        self.LAYOUT = Horizontal(
-                                Input(placeholder="", id="Search-field"),
-                                Button("[] Search", id="Search-button"),
+                this.LAYOUT = Vertical(
+                        # Widget Header
+                        Static(f"{this.CONFIG.HEADER}:",
+                               classes="dialogue-header-top"),
 
-                                classes="w-fill",
+                        # Input Form (Box + Button)
+                        Horizontal(
+                                Input(placeholder=f"[{this.CONFIG.HINT_ICON}"
+                                                  f"]{this.CONFIG.HEADER}",
+                                        id="Search-field"),
+                                Button(f"[{this.CONFIG.SEARCH_ICON}] Search",
+                                        id="Search-button"),
+
                                 id="Search-widget"
-                        )
-                else:
-                        log("log", f"IForm __build(): no config")
-                        # Todo: Fix bug where defaults are not initialized
-                        # Todo: Defaults system
-                        self.LAYOUT = InputForm.__parse_cfg(InputForm.CONFIG)
+                        ),
+                        classes="h-auto-w-fill"
+
+                )
+                # if self.CONFIG != None:
+                #         log("log", f"IForm __build(): has config: "
+                #                    f"XX")
+                #         self.LAYOUT = Horizontal(
+                #                 Input(placeholder="", id="Search-field"),
+                #                 Button("[] Search", id="Search-button"),
+                #
+                #                 classes="w-fill",
+                #                 id="Search-widget"
+                #         )
+                # else:
+                #         log("log", f"IForm __build(): no config")
+                #         # Todo: Fix bug where defaults are not initialized
+                #         # Todo: Defaults system
+                #         self.LAYOUT = InputForm.__parse_cfg(InputForm.CONFIG)
 
         def compose(self):
                 yield self.LAYOUT
