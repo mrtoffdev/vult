ef0eb98d8a36093f9b3749097cb39035aad60888
mrtoffdev <pub.mrtoff@proton.me>
2023-08-08 20:29:25 +0800

    Refactor: Use new Explorer widget definition after major construction pipeline refactors. Supports using widget defaults instead of specifying configurations during def/init

--- src/view/fm_panel.py
+++ src/view/fm_panel.py
@@ -2,9 +2,8 @@ from core.typedef import vec_T_str
 
 from textual.app import ComposeResult
 from textual.widgets import Static
-from textual.containers import Container, Vertical
+from textual.containers import Vertical
 
-from view.components.InputForm import InputForm
 from view.components.Explorer import Explorer
 
 from util.dev_utils import log
@@ -12,7 +11,7 @@ from core.vult_core import Core
 
 class FileChooserDialogue(Static):
         # State
-        SOURCES: vec_T_str      = [("", "")]
+        SOURCES: vec_T_str      = vec_T_str
 
         # DOM
         CSS_PATH                = "compress-scr.css"
@@ -21,53 +20,30 @@ class FileChooserDialogue(Static):
         def rebuild_sources(self, source: str):
                 self.SOURCES = Core.build_sources(source)
 
+        def __construct(this):
+                this.LAYOUT     = Vertical(
+                        # Sources Explorer
+                        Explorer(
+                                classes="w-fill h-half-p dbg-1",
+                                 id="source-panel"
+                        ),
+
+                        # Output Explorer
+                        Explorer(
+                                classes="w-fill h-half-p dbg-1",
+                                 id="output-panel"
+                        ),
+
+                        classes="pad-s"
+                )
+
         def __init__(self):
                 super().__init__()
                 log("log", "Init reached")
 
-                # Upper Section
-                SOURCE_TREE     = Explorer(("File Name", "Size"), [("", "")])
-                SOURCE_TREE.id  = "Source-dir"
-
                 self.rebuild_sources("./tests/")
+                self.__construct()
                 log("logfile.txt", self.SOURCES)
-                SOURCE_TREE.init_table((str("File Name"), str("Size")), self.SOURCES)
-
-
-                # Lower Section
-                OUT_TREE        = Explorer(("File Name", "Size"), [("", "")])
-                OUT_TREE.id     = "Out-dir"
-
-                OUT_TREE.init_table(("", ""), [("", "")])
-
-                self.LAYOUT     = Vertical(
-                        # Video Sources [Upper Section]
-                        Vertical(
-                                # Static("Video Sources Directory:", classes="dialogue-header-top"),
-                                InputForm(config={
-                                        "header": "[] View Sources Directory:",
-                                        "s_icon": '',
-                                        "hint"  : "[] Directory Location:",
-                                        "h_icon": ''
-                                }),
-                                SOURCE_TREE,
-
-                                classes="w-fill h-half-p dbg-1",
-                        ),
-
-                        # Output & Statistics [Lower Section]
-                        Vertical(
-                                # Static( "Output Directory:",
-                                #         classes="dialogue-header-sub"),
-                                InputForm(),
-
-                                Static("Results:",
-                                        classes="dialogue-header-sub"),
-                                OUT_TREE,
-
-                                classes="FCD-section",
-                        )
-                )
 
         def compose(self) -> ComposeResult:
                 yield self.LAYOUT
