47383fd8c9cc161965ff9afcf3951a51b886bac5
mrtoffdev <pub.mrtoff@proton.me>
2023-08-15 05:11:19 +0800

    Tests: Test selection menu during start

--- tests/tui_elem.py
+++ tests/tui_elem.py
@@ -3,6 +3,8 @@ import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 from textual.app import App, ComposeResult
+from textual.widgets import Static
+
 from vult.view.components.InputForm import InputForm
 
 # InputForm -----
@@ -18,5 +20,16 @@ class TableTest(App[str]):
 
 if __name__ == '__main__':
         # app = InputFormTest(css_path="../vult/compress-scr.css")
-        app = TableTest(css_path="../vult/compress-scr.css")
+        select = input("Run Test:\n"
+                       "1. InputForm\n"
+                       "2. Table\n")
+
+        select = int(select)
+        app = Static("Test Selection Err")
+
+        if select == 1:
+                app = InputFormTest(css_path="../vult/compress-scr.css")
+        elif select == 2:
+                app = TableTest(css_path="../vult/compress-scr.css")
+
         print(app.run())
