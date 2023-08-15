f75d7068f8537adfb814d2fe6137e62bcba0bd78
mrtoffdev <pub.mrtoff@proton.me>
2023-08-15 23:59:02 +0800

    Tests: Rewrite Tests

---
+++ tests/test_tui_elem.py
@@ -0,0 +1,39 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
+from textual.app import App, ComposeResult
+
+
+CSS_PATH = "../vult/compress-scr.css"
+
+
+# InputForm ----------
+from vult.view.components.InputForm import InputForm
+def test_InputForm():
+
+        class InputFormTest(App[str]):
+                def compose(self) -> ComposeResult:
+                        yield InputForm()
+
+        print("InputForm")
+        app = InputFormTest(css_path=CSS_PATH)
+        print(app.run())
+
+
+
+# Table --------------
+from vult.view.components.Table import Table
+def test_Table():
+
+        class TableTest(App[str]):
+
+                def compose(self) -> ComposeResult:
+                        yield Table()
+
+        print("Table")
+        app = TableTest(css_path=CSS_PATH)
+        print(app.run())
+
+if __name__ == '__main__':
+        test_Table()
