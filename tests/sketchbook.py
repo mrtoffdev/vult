5c680a3f922e27c119f813fb25ffad41047e5770
mrtoffdev <pub.mrtoff@proton.me>
2023-09-04 12:39:00 +0800

    Chore: Updated TODO list. FFMPEG integration phase

--- tests/sketchbook.py
+++ tests/sketchbook.py
@@ -1,4 +1,13 @@
 
+'''
+Todo:
+1. find bindings for ffmpeg-cli commands in ffmpeg-python library
+2. include support for off-standard codec + format combinations (e.g. h265 enc in .mov)
+3. bind textual frontend to vult_core ffmpeg operations
+        A. CRF auto adjustment in backend + supporting sliders / group radio button
+        B. Resolution auto adjustment to occupy only frame required space instead of
+                container required space
+'''
 def prefix():
         input   = "./test"
         output: str
